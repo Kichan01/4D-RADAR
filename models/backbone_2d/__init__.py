255b40e80d5a19bd4e40f0db478aff1e1bf7f410
Kichan01 <87978447+Kichan01@users.noreply.github.com>
2023-04-15 12:39:36 +0900

    Update __init__.py

--- models/backbone_2d/__init__.py
+++ models/backbone_2d/__init__.py
@@ -1,6 +1,7 @@
-from .pillars_backbone import PillarsBackbone
+#현재 디렉토리의 from. 모듈에서 import 한 클래스를 가져와서 사용
+from .pillars_backbone import PillarsBackbone 
 from .resnet_wrapper import ResNetFPN
-
+#__all__ : 해당 객체를 외부에서 사용 가능하도록 노출시킴. 
 __all__ = {
     'PillarsBackbone': PillarsBackbone,
     'ResNetFPN': ResNetFPN,
