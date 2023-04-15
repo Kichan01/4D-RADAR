d8833c423073505cb0636f986e78b9afe6dd91a2
Kichan01 <87978447+Kichan01@users.noreply.github.com>
2023-04-15 11:27:53 +0900

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,6 @@
 from .kradar_detection_v1_0 import KRadarDetection_v1_0
 # from .kradar_detection_v1_1 import KRadarDetection_v1_1
-
+#all 변수를 설정하여 모듈 외부에서 "KRadarDetection_v1_0" 클래스에 직접적으로 접근할 수 있도록 설정
 __all__ = {
     'KRadarDetection_v1_0': KRadarDetection_v1_0,
     # 'KRadarDetection_v1_1': KRadarDetection_v1_1,
